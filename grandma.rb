890c9b46535fe492c050b6a48fbe48246b9ec935
Carl J <carlj1881@yahoo.com>
2020-09-11 00:07:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ ref speak_to_grandma(string)
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
   elsif string.upcase && string == "I LOVE YOU GRANDMA!"
-    return 
+    return "I LOVE YOU TOO PUMPKIN!
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
