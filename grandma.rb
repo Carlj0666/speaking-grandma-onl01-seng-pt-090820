c980851cd2d1431f54234d13be945b13885709aa
Carl J <carlj1881@yahoo.com>
2020-09-11 00:04:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
 # Write a speak_to_grandma method.
-
+ref speak_to_grandma(string)
+  if string
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif string.upcase
+    puts ""
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
