b91deebdbb0c2aca5e200fa5867ec48953190722
Carl J <carlj1881@yahoo.com>
2020-09-11 00:20:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
-  
+  end
   end
 end
 # Whatever you say to grandma, she should respond with
